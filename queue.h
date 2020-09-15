a4d9cb2f10ef796115fbdf4b8b1cf8d799e3b730
Wei Cheng <michael1996623@gmail.com>
2020-09-15 13:40:16 +0800

    Add tail and size field to a queue
    To efficiently implement the q_size and q_insert_tail function,
    tail and size field is added to the queue_t struct

--- queue.h
+++ queue.h
@@ -24,11 +24,9 @@ typedef struct ELE {
 
 /* Queue structure */
 typedef struct {
-    list_ele_t *head; /* Linked list of elements */
-    /* TODO: You will need to add more fields to this structure
-     *        to efficiently implement q_size and q_insert_tail.
-     */
-    /* TODO: Remove the above comment when you are about to implement. */
+    list_ele_t *head;  /* Linked list of elements */
+    list_ele_t *tail;  /* Linked list of elements */
+    unsigned int size; /* size of the queue*/
 } queue_t;
 
 /* Operations on queue */
