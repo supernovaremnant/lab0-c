5b79de88a377de298d21769f7005777fba17bc4a
Wei Cheng <michael1996623@gmail.com>
2020-09-15 14:05:48 +0800

    Implement the q_free function
    This function should free all allocated memory to the queue.
    Including all list elements and the strings

--- queue.c
+++ queue.c
@@ -29,8 +29,16 @@ queue_t *q_new()
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
-    /* TODO: How about freeing the list elements and the strings? */
-    /* Free queue structure */
+    if (!q)
+        return;
+
+    while (q->head) {
+        list_ele_t *tmp = q->head;
+        q->head = q->head->next;
+        free(tmp->value);
+        free(tmp);
+    }
+
     free(q);
 }
 
